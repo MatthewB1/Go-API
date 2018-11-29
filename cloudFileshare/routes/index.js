378fd1c973bbcbcb59f34511984ca138233b2c1a
MatthewB1 <Matthewjball96@gmail.com>
2018-11-29 18:11:32 +0000

    creating project - doesn't work

---
+++ cloudFileshare/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hopefully we\'re going to output some data here', });
+});
+
+module.exports = router;
