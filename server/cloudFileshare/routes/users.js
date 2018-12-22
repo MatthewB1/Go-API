5dc71858aa8cf4eaac9f103fbc0db76a1a5e6e1d
MatthewB1 <Matthewjball96@gmail.com>
2018-12-22 20:17:45 +0000

    fixed resource routing, TODO fix creating a new user

--- server/cloudFileshare/routes/users.js
+++ server/cloudFileshare/routes/users.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'db';
-const client = new MongoClient(url);
+const client = new MongoClient(url, {useNewUrlParser : true});
 
 
 var express = require('express');
@@ -11,22 +11,24 @@ var multer = require('multer');
 var upload = multer();
 
 /* GET users listing. */
-router.get('/users/getAll', function (req, res, next) {
+router.get('/getAll', function (req, res, next) {
   console.log("getAll hit!");
   client.connect(function (err) {
     const db = client.db(dbName);
     const collection = db.collection('users');
     collection.find({}).toArray(function (err, data) {
       if (err)
-        return res.status(false).json(err);
+        return res.json({success : false, error: err});
       else
-        return res.status(true).json(data);
+        return res.json({ success: true, data: data });
     });
   });
 });
 
-router.post('/users/addUser', upload.array(), function (req, res) {
-  let newUser = { username: req.body.username, password: req.body.password };
+router.post('/add', upload.array(), function (req, res) {
+  let newUser = { username: req.body.username , password: req.body.password };
+
+  //adding a user does not work properly
 
   client.connect(function (err) {
     const db = client.db(dbName);
